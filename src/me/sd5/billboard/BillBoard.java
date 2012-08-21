e5b0784209dac5d75338eb56f917ad8f5e32acfb
sd5 <timo.braendlin@googlemail.com>
2012-08-22 00:48:16 +0200

    Added a method for adding an advertising to the billboard.

--- src/me/sd5/billboard/BillBoard.java
+++ src/me/sd5/billboard/BillBoard.java
@@ -1,5 +1,8 @@
 package me.sd5.billboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * @author sd5
@@ -14,4 +17,17 @@ package me.sd5.billboard;
  */
 public class BillBoard {
 
+	private static List<Advertising> board = new ArrayList<Advertising>();
+	
+	/**
+	 * Adds an advertising to the billboard.
+	 * @param advertising:
+	 *   The advertising to add.
+	 */
+	public static void add(Advertising advertising) {
+		
+		board.add(advertising);
+		
+	}
+	
 }
