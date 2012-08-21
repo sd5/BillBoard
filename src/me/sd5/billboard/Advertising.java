bc7a0f3ef5e9e86deb1f999a471d8d14966f6217
sd5 <timo.braendlin@googlemail.com>
2012-08-22 00:28:08 +0200

    Added constructor.

--- src/me/sd5/billboard/Advertising.java
+++ src/me/sd5/billboard/Advertising.java
@@ -11,4 +11,26 @@ package me.sd5.billboard;
  */
 public class Advertising {
 
+	private String player;
+	private String date;
+	private String message;
+	
+	/**
+	 * Creates a new advertising.
+	 * @param player:
+	 *   The player who created the advertising.
+	 * @param date:
+	 *   The date when the advertising was created.
+	 *   Date format is configurable in config.
+	 * @param message:
+	 *   The message of the advertising.
+	 */
+	public Advertising(String player, String date, String message) {
+		
+		this.player = player;
+		this.date = date;
+		this.message = message;
+		
+	}
+	
 }
